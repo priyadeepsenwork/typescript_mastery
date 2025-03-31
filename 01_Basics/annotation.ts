3f745bf4acd1b83acf40086f4094975585a6c5f9
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-03-31 23:07:49 +0530

    first commit

---
+++ 01_Basics/annotation.ts
@@ -0,0 +1,17 @@
+//* Annotations in Typescript
+
+//string
+let myName: string = "Priyadeep Sen"
+//myName = 69  * type number is not assignable to type string
+myName = "Javascript Mastery"
+console.log(myName)
+
+//number
+let favNumber: number = 7
+favNumber += 2
+console.log(favNumber)
+
+//boolean
+let tsHard: boolean = false
+tsHard = true
+console.log(tsHard)
---
+++ 01_Basics/type_Inference.ts
@@ -0,0 +1,21 @@
+/* 
+Its a technique where the TS-Compiler
+automatically determine the type of the
+variable based on its value.
+*/
+
+let tech = "Typescript";
+//* tech = 19  --> this will WORK in Javascript
+tech = "Priyadeep Sen"
+console.log(tech)
+console.log(typeof tech)
+
+
+let str = "JS Mastery"
+let nomm = 1000
+let isChecked = true
+
+console.log(str)
+console.log(nomm)
+console.log(isChecked)
+
